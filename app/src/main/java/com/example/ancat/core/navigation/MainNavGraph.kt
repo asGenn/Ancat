5ab1ee407d9644794ca679ca42373aca91766c36
yakup <yakuphan40@gmail.com>
2024-10-29 22:18:51 +0300

    json düzenlendi, çoktan seçmeli soru eklendi, sorunun data class'ı oluşturuldu.

--- app/src/main/java/com/example/ancat/core/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/ancat/core/navigation/MainNavGraph.kt
@@ -6,13 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.example.ancat.ui.component.FloatingMenuScreen
 import com.example.ancat.ui.views.CreateScreen
 import kotlinx.serialization.Serializable
--- app/src/main/java/com/example/ancat/survey/QuestionsHelper.kt
+++ app/src/main/java/com/example/ancat/survey/QuestionsHelper.kt
@@ -2,45 +2,75 @@ package com.example.ancat.survey
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import com.example.ancat.data.MultipleChoiceQuest
 
 class QuestionsHelper {
 
     fun ratingQuestion(
         canvas: Canvas,
         paint: Paint,
-        question: String,
-        cursorPos: Float,
+        questions: List<String>,
+        cursorPosition: Float,
     ): Float {
 
         val questionWidth = 430f  // Soru sütunu genişliği
         val optionWidth = 21f     // Puan sütunu genişliği (5, 4, 3, 2, 1)
         val cellHeight = 30f
 
-        // Metni dikey olarak ortala
+        var cursorPos = cursorPosition
+
         val start = 20f
         val end = 575f
-
         val textX = 30f
-        val textY = cursorPos + (cellHeight + paint.textSize) / 2
 
-        // Soruyu çiz
-        canvas.drawText(question, textX, textY, paint)
+        questions.forEach { question ->
+            val textY = cursorPos + (cellHeight + paint.textSize) / 2
+            canvas.drawText(question, textX, textY, paint)
+            for (i in 1..5)
+                canvas.drawText(i.toString(), questionWidth + i * optionWidth, textY, paint)
+            canvas.drawLine(start, cursorPos, end, cursorPos, paint)
+            canvas.drawLine(start, cursorPos, start, cursorPos + cellHeight, paint)
+            canvas.drawLine(start, cursorPos + cellHeight, end, cursorPos + cellHeight, paint)
+            canvas.drawLine(end, cursorPos, end, cursorPos + cellHeight, paint)
+            cursorPos += cellHeight
+        }
+        return cursorPos + 20f // Sorular arası boşluk
+    }
 
-        for (i in 1..5)
-            canvas.drawText(i.toString(), questionWidth + i * optionWidth, textY, paint)
+    fun multipleChoiceQuestion(
+        canvas: Canvas,
+        paint: Paint,
+        questions: List<MultipleChoiceQuest>,
+        cursorPosition: Float,
+    ): Float {
 
-        canvas.drawLine(start, cursorPos, end, cursorPos, paint) // Üst kenar
-        canvas.drawLine(start, cursorPos, start, cursorPos + cellHeight, paint) // Sağ kenar
-        canvas.drawLine(
-            start,
-            cursorPos + cellHeight,
-            end,
-            cursorPos + cellHeight,
-            paint
-        ) // Alt kenar
-        canvas.drawLine(end, cursorPos, end, cursorPos + cellHeight, paint) // Sol kenar
+        val questionWidth = 380f  // Soru sütunu genişliği
+        val cellHeight = 30f
 
-        return cursorPos + cellHeight
+        var cursorPos = cursorPosition
 
+        val start = 20f
+        val end = 575f
+        val textX = 30f
+
+        questions.forEach { arr ->
+            var textY = cursorPos + (cellHeight + paint.textSize) / 2
+            canvas.drawText(arr.question, textX, textY, paint)
+            cursorPos += (arr.options.size - 1) * 30f
+
+            arr.options.forEachIndexed { index, option ->
+                canvas.drawText("${index+1}. (     )   $option", questionWidth, textY, paint)
+                textY += 30f
+            }
+
+            canvas.drawLine(start, cursorPosition, end, cursorPosition, paint) // üst
+            canvas.drawLine(end, cursorPosition, end, cursorPos + cellHeight, paint) // Sağ
+            canvas.drawLine(start, cursorPos + cellHeight, end, cursorPos + cellHeight, paint) // Alt
+            canvas.drawLine(start, cursorPosition, start, cursorPos + cellHeight, paint) // Sol
+
+            cursorPos += cellHeight
+        }
+
+        return cursorPos + 20f // Sorular arası boşluk
     }
 }
