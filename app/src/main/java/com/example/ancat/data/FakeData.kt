84d0d2a2fddd810c763cf18716883767e00f911b
yakup <yakuphan40@gmail.com>
2024-10-31 16:09:43 +0300

    Yeni sayfaya taşma eklendi. (Kod kötü yazıldı düzeltilecek!)

---
+++ app/src/main/java/com/example/ancat/data/FakeData.kt
@@ -0,0 +1,81 @@
+package com.example.ancat.data
+
+
+/**
+ * ÖRNEK DATA
+ */
+
+val jsonData = """
+            [
+                {
+                    "type": "0",
+                    "title": "Anket Başlığı",
+                    "questions": [
+                        "Açıklama 1",
+                        "Açıklama 2",
+                        "Açıklama 3"
+                    ]
+                },
+                {
+                    "type": "1",
+                    "title": "Soru 1 Başlığı",
+                    "questions": [
+                        "Soru 1",
+                        "Soru 2",
+                        "Soru 3",
+                        "Soru 4",
+                        "Soru 5",
+                        "Soru 6",
+                        "Soru 1",
+                        "Soru 2",
+                        "Soru 3",
+                        "Soru 4",
+                        "Soru 5",
+                        "Soru 6"
+                    ]
+                },
+                {
+                    "type": "2",
+                    "title": "Soru 2 Başlığı",
+                    "questions": [
+                        {
+                            question: "Soru1",
+                            options: [
+                                "Şık1",
+                                "Şık2"
+                            ]
+                        },
+                        {
+                            question: "Soru2",
+                            options: [
+                                "Şık1",
+                                "Şık2",
+                                "Şık3"
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "1",
+                    "title": "Soru 1.1 Başlığı",
+                    "questions": [
+                        "Soru 1",
+                        "Soru 2",
+                        "Soru 3",
+                        "Soru 4",
+                        "Soru 5",
+                        "Soru 6",
+                        "Soru 1",
+                        "Soru 2",
+                        "Soru 3",
+                        "Soru 4",
+                        "Soru 5",
+                        "Soru 6"
+                    ]
+                }
+            ]
+        """.trimIndent()
+
+/**
+ * ÖRNEK DATA BİTİŞ
+ */
