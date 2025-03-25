36d2d32e44336ff37f2f85d9e048081148c7f238
yakup <yakuphan40@gmail.com>
2025-03-25 05:10:47 +0300

    Test verisi değişikliklere uygun hale getirildi.

--- app/src/main/java/edu/aibu/ancat/core/renderer/survey_drawings/drawer/TitleAndCommits.kt
+++ app/src/main/java/edu/aibu/ancat/core/renderer/survey_drawings/drawer/TitleAndCommits.kt
@@ -1,18 +1,22 @@
 package edu.aibu.ancat.core.renderer.survey_drawings.drawer
 
 import android.graphics.Canvas
+import android.util.Log
+import edu.aibu.ancat.core.renderer.survey_drawings.utils.TextHandler
 import edu.aibu.ancat.utils.DocumentConstants.MARGIN
 import edu.aibu.ancat.utils.DocumentConstants.PAGE_WIDTH
 import edu.aibu.ancat.utils.DocumentConstants.TITLE_PADDING
 import edu.aibu.ancat.utils.PaintFactory
 import edu.aibu.ancat.data.model.Question
+import edu.aibu.ancat.utils.DocumentConstants.CELL_HEIGHT
 import edu.aibu.ancat.utils.DocumentConstants.START_CURSOR
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class TitleAndCommits @Inject constructor(
-    private val paintFactory: PaintFactory
+    private val paintFactory: PaintFactory,
+    private val textHandler: TextHandler
 ) {
 
     fun surveyTitleCommit(
@@ -24,11 +28,22 @@ class TitleAndCommits @Inject constructor(
 
         val titleCenter = (PAGE_WIDTH - paintFactory.title().measureText(commits.title)) / 2
         canvas.drawText(commits.title, titleCenter, cursorPos, paintFactory.title())
-        cursorPos += TITLE_PADDING
+
+        cursorPos += MARGIN * 3
 
         commits.description.forEach { commit ->
-            canvas.drawText(commit, MARGIN * 3, cursorPos, paintFactory.text())
-            cursorPos += MARGIN * 2
+            val textList: List<String> = textHandler.getWrappedText(
+                text = commit,
+                paint = paintFactory.text(),
+                xCursor = MARGIN * 3,
+                maxWidth = PAGE_WIDTH - MARGIN * 3
+            )
+
+            textList.forEach {
+                cursorPos += TITLE_PADDING
+                canvas.drawText(it, MARGIN * 3, cursorPos, paintFactory.text())
+            }
+            cursorPos += MARGIN
         }
         return cursorPos
     }
@@ -40,10 +55,20 @@ class TitleAndCommits @Inject constructor(
     ): Float {
 
         var cursorPos = cursorPosition
-        descriptions.description.forEach {
-            canvas.drawText(it, MARGIN * 3, cursorPos, paintFactory.text())
-            cursorPos += TITLE_PADDING
+
+        descriptions.description.forEach { text ->
+            val textList: List<String> = textHandler.getWrappedText(
+                text = text,
+                paint = paintFactory.text(),
+                xCursor = MARGIN * 3,
+                maxWidth = PAGE_WIDTH - MARGIN * 3
+            )
+
+            textList.forEach {
+                canvas.drawText(it, MARGIN * 3, cursorPos, paintFactory.text())
+                cursorPos += CELL_HEIGHT
+            }
         }
-        return cursorPos
+        return cursorPos + TITLE_PADDING
     }
 }
