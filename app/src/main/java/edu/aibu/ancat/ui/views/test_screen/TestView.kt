ccc74fdbc09bfbb82823b3bcd5308c8a080b9df4
yakup <yakuphan40@gmail.com>
2025-04-08 00:54:46 +0300

    Test bug fix

--- app/src/main/java/edu/aibu/ancat/ui/views/test_screen/TestView.kt
+++ app/src/main/java/edu/aibu/ancat/ui/views/test_screen/TestView.kt
@@ -1,5 +1,6 @@
 package edu.aibu.ancat.ui.views.test_screen
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -8,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import edu.aibu.ancat.core.helper.JsonHelper
+import edu.aibu.ancat.domain.entity.JsonFilesInfoEntity
 import edu.aibu.ancat.ui.views.create_screen.CreateScreenViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -28,17 +30,33 @@ fun TestView() {
         Button(
             onClick = { CoroutineScope(Dispatchers.Main).launch {
 
+                // Saving JSON to file
                 val path = JsonHelper().saveJsonToFile(
                     context = context,
                     fileName = "TestData",
                     jsonData = Json.encodeToString(viewModel.data)
                 )
 
-                viewModelSec.saveJsonFileToDB(
-                    fileName = "TestData",
-                    filePath = path,
-                    title = "Test Anketi"
-                )
+                // Fetching existing files from DB
+                val data: List<JsonFilesInfoEntity> = viewModelSec.getJsonFiles()
+
+                // Check if the file already exists in the database
+                val fileExists = data.any { it.fileName == "TestData" }
+
+                if (fileExists) {
+                    // Show a toast if the file already exists
+                    Toast.makeText(context, "Already exists", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Save the file to the database if it doesn't exist
+                    viewModelSec.saveJsonFileToDB(
+                        fileName = "TestData",
+                        filePath = path,
+                        title = "Test Anketi"
+                    )
+                    // Show a toast that the file was added successfully
+                    Toast.makeText(context, "Added successfully", Toast.LENGTH_SHORT).show()
+                }
+
             } }
         ) {
             Text(text = "Test Anketi Ekle")
